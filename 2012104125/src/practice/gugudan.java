9075e8552b6d314f27dd2a8c38c9c5187b27de05
Cho Jae-Ho <hydism@nate.com>
2019-04-12 10:21:57 +0900

    practice2012104125

---
+++ 2012104125/src/practice/gugudan.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 8; i++) {
+			System.out.println("----" + i + "단----");
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + " X " + j + " = " + i * j);
+			}
+		}
+	}
+}
